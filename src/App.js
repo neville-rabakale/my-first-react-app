f216e2b0526b9bd1fc8591c0a34322cf83897fb0
Neville Rabakale <neville.rabakale@gmail.com>
2022-11-29 20:56:19 +0100

    Playing around with properties and elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Article(props){
+
+  return (
+    <div className='Article'>
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.author} | {props.published}</p>
+      </div>
+    <div className="Article_Body">{props.children}</div>
+    </div>
+  );
+}
+
 function App() {
+  const name = "Neville";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is {name} and this is my first app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <Article title="Learning React" author="Viktoria Rabakale" published="1990">
+      <p>This is a child compenent</p>
+      <p>I can add more than one at a time</p>
+      </Article>
+      <Article title="Learning JSX" author="John Snow" published="2020">
+        <p>This is another one</p>
+      </Article>
+      <Article title= {`${name} is my name`} author ="Lena Lidsell" published="1967"/>
+       </div>
   );
 }
+
 
 export default App;
